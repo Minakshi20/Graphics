4005fedb0d2546a7b04123a4bbddb9a11f281d70
Minakshi Sontakke <78152858+Minakshi20@users.noreply.github.com>
2023-12-10 22:30:04 +0530

    Add files via upload

---
+++ design.py
@@ -0,0 +1,17 @@
+import turtle as tur
+tur.bgcolor('black')
+tur.tracer(1)
+tur.pensize(2)
+for i in range(50):
+
+         
+    tur.pencolor('cyan')
+    tur.circle(100)
+    tur.left(50)
+    tur.circle(70)
+    tur.right(100)
+    tur.circle(40)
+    tur.left(20)
+    tur.right(50)
+    tur.circle(200)
+done()
